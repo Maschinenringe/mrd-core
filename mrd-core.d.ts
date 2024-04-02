ad014abff47e494d61f965d1f26fd1d69ef71723
Benjamin Klein <bk@fub.de>
2024-04-02 13:51:18 +0200

    version update

--- mrd-core.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-export { ReadonlyRestServiceObserver as ɵb } from './lib/impl/http/readonly-rest-service-observer';
-export { RestServiceObserver as ɵa } from './lib/impl/http/rest-service-observer';
